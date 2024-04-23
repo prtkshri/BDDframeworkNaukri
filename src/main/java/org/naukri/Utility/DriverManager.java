f9433af5aad71bfa859719aa953391cd9570b77c
prtkshri <prateek1903@gmail.com>
2024-04-23 10:09:42 +0530

    beauity code

--- src/main/java/org/naukri/Utility/DriverManager.java
+++ src/main/java/org/naukri/Utility/DriverManager.java
@@ -1,20 +1,8 @@
 package org.naukri.Utility;
 
-import com.aventstack.extentreports.ExtentReports;
-import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
-import com.aventstack.extentreports.ExtentTest;
-import com.aventstack.extentreports.Status;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.ITestResult;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-
-import java.io.FileInputStream;
 import java.time.Duration;
-import java.util.Properties;
-
 
 public class DriverManager {
 
@@ -34,14 +22,6 @@ public class DriverManager {
             driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(Page_Load_Timeout));
         }
         return driver;
-    }
-
-    public static WebDriver getDriver() {
-        return driver;
-    }
-
-    public static void setDriver(WebDriver driver) {
-        DriverManager.driver = driver;
     }
 
     public void openPage(String url, WebDriver driver) {
